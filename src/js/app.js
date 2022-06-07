f5149aafb95ec05600015ee8addb432f93b3eb05
Ilgiz <ilgfaizyl.ilgiz@yandex.ru>
2022-06-07 23:39:00 +0500

    tabs

--- src/js/app.js
+++ src/js/app.js
@@ -20,3 +20,16 @@ new Swiper(".product-tab", {
           prevEl: ".swiper-button-prev",
         },
 });
+
+$(function () {
+  
+  $('.tab').on('click', function(e){
+    e.preventDefault();
+  
+          $($(this).siblings()).removeClass('tab--active');
+          $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+          
+          $(this).addClass('tab--active');
+          $($(this).attr('href')).addClass('tabs-content--active');
+  });
+});
